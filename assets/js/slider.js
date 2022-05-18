36c83db5b3d939a9845cd70fa8e0d4ab66bb1d80
Tommy Meyer <tommymeyer@Tommys-MacBook-Pro.local>
2022-05-17 17:31:22 -0700

    Adds images and creates PDPs.

---
+++ assets/js/slider.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    // autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    infinite: true,
+    lazyLoad: 'ondemand',
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
